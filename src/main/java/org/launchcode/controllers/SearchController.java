d7e32a6cbe697ae8910ed8daa6d2de30a14018af
Bill Volz <william.volz13@gmail.com>
2017-12-04 10:17:55 -0600

    finished mvp

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -3,6 +3,7 @@ package org.launchcode.controllers;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,6 +23,22 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
 
+    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String searchresults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList jobs;
+
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        String jobSize = jobs.size() + "Results";
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("jobSize", jobSize);
+
+        return "search";
+    }
 }
